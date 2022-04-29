09057cecde70489a2c1586b2fea9ba1bc3951776
Eric Sexton <foopis23@gmail.com>
2022-04-29 10:57:07 -0400

    added ability to input ip for debugging

--- src/client/main.ts
+++ src/client/main.ts
@@ -3,11 +3,25 @@ import './style.css'
 import { ClientEngine } from './client-engine'
 
 function start() {
+  const location = prompt('Enter IP:PORT')
+  if (location == null) {
+    return
+  }
+
+  const [ip, port] = location.split(':')
+  const portNum = parseInt(port)
+
+  if (isNaN(portNum)) {
+    alert('Invalid IP:PORT')
+    return
+  }
+  
   const el = document.getElementById('main-menu');
   if (el) {
     el.style.display = 'none'
   }
-  new ClientEngine('localhost', 'ws', 9500)
+
+  new ClientEngine(ip, 'ws', portNum)
 }
 
 document.getElementById('start-button')?.addEventListener('click', start)
